22a08b2b4ed3307c5c5c17e387d4075d6b850c3c
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-24 16:59:20 +0800

    搜索功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import { shikiPlugin } from "@vuepress/plugin-shiki";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -14,6 +15,21 @@ export default defineUserConfig({
     shikiPlugin({
       // 你的选项
       theme: "one-dark-pro",
+    }),
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
     }),
   ],
   // Enable it with pwa
