e6d6b88bcdc462a96a24d2ef5ea2babb3a33a223
皮皮萌宝 <panda1943575780@outlook.com>
2023-11-24 16:56:09 +0800

    代码高亮

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { shikiPlugin } from "@vuepress/plugin-shiki";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -9,7 +10,12 @@ export default defineUserConfig({
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
-
+  plugins: [
+    shikiPlugin({
+      // 你的选项
+      theme: "one-dark-pro",
+    }),
+  ],
   // Enable it with pwa
   // shouldPrefetch: false,
 });
